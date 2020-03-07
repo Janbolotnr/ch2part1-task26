60c6603c32ac3486890b0d397a2c0b0029444dfe
Janbolotnr <janbolotnr@gmail.com>
2020-03-08 02:02:58 +0600

    ch2task26

---
+++ decision26.py
@@ -0,0 +1,20 @@
+import random
+
+a = []
+for i in range(10):
+    a.append(int(random.random() * 100))
+print (a)
+
+even = 0
+odd = 0
+
+for i in a:
+    if i % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+print("Even", even)   
+print("Odd", odd)
+
+
+
